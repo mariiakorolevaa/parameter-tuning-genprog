a227014890d34094df0a48c41d192447b094caf5
Mariia Koroleva <42766063+mariiakorolevaa@users.noreply.github.com>
2024-01-20 04:36:41 +0100

    coverage in combined fitness

--- fitness/fitness_functions.py
+++ fitness/fitness_functions.py
@@ -5,6 +5,7 @@ from tools.json_utils import JsonUtils
 from tools.cmd_utils import run_cmd_and_get_fitness
 
 best_fitness = 1000000
+best_coverage = 0
 stopping_criteria_reached = False
 best_params = [0, 0, 0, 0]
 time_for_best_params = 0
@@ -38,7 +39,7 @@ def get_iteration():
 def fitness_function(parameters: FitnessParameters):
     global best_fitness, best_params, population_size, \
         elitism_size, crossover_rate, mutation_rate, \
-        mutation_insertion_rate, mutation_deletion_rate, mutation_change_rate, time_for_best_params
+        mutation_insertion_rate, mutation_deletion_rate, mutation_change_rate, time_for_best_params, best_coverage
 
     json_utils = JsonUtils(parameters.general_parameters.path_to_config)
     if parameters.general_parameters.is_rationals:
@@ -81,16 +82,18 @@ def fitness_function(parameters: FitnessParameters):
         json_utils.replace_json_int(population_size, elitism_size)
 
     start_time = time.time()
-    fitness, timee = run_cmd_and_get_fitness(parameters.general_parameters)
+    fitness, coverage = run_cmd_and_get_fitness(parameters.general_parameters)
     end_time = time.time()
     exec_time = end_time - start_time
     if exec_time == 0:
         return best_fitness, time_for_best_params
     print("fitness: ", fitness)
+    print("coverage: ", coverage)
     print("time: ", exec_time)
-    combined_fitness = exec_time / fitness
-    if combined_fitness < best_fitness:
+    combined_fitness = exec_time / (fitness + coverage)
+    if combined_fitness <= best_fitness and coverage >= best_coverage:
         best_fitness = combined_fitness
+        best_coverage = coverage
         if not parameters.general_parameters.is_rationals:
             best_params[0] = population_size
             best_params[1] = elitism_size
@@ -111,7 +114,8 @@ def fitness_function(parameters: FitnessParameters):
 def fitness_function_de(x, *args):
     global best_fitness, best_params, population_size, \
         elitism_size, crossover_rate, mutation_rate, \
-        mutation_insertion_rate, mutation_deletion_rate, mutation_change_rate, time_for_best_params, iteration
+        mutation_insertion_rate, mutation_deletion_rate, mutation_change_rate, time_for_best_params, \
+        iteration, best_coverage
 
     global stopping_criteria_reached
 
@@ -161,12 +165,13 @@ def fitness_function_de(x, *args):
 
     iteration += 1
     start_time = time.time()
-    fitness, timee = run_cmd_and_get_fitness(parameters.general_parameters)
+    fitness, coverage = run_cmd_and_get_fitness(parameters.general_parameters)
     end_time = time.time()
     exec_time = end_time - start_time
-    combined_fitness = exec_time / fitness
-    if combined_fitness < best_fitness:
+    combined_fitness = exec_time / (fitness + coverage)
+    if combined_fitness <= best_fitness and coverage >= best_coverage:
         best_fitness = combined_fitness
+        best_coverage = coverage
         if not parameters.general_parameters.is_rationals:
             best_params[0] = population_size
             best_params[1] = elitism_size
--- tools/cmd_utils.py
+++ tools/cmd_utils.py
@@ -46,11 +46,11 @@ def run_cmd_and_get_fitness(parameters):
             print(e)
             return 1
 
-        best_fitness, time = sort_by_fitness_and_time(df)
+        best_fitness, coverage = sort_by_fitness_and_coverage(df)
 
     else:
         try:
-            fitness_time_pairs = []
+            fitness_coverage_pairs = []
             experiments_path = parameters.path_to_csv
             experiment_folders = [folder for folder in os.listdir(experiments_path) if
                                   os.path.isdir(os.path.join(experiments_path, folder))]
@@ -58,48 +58,48 @@ def run_cmd_and_get_fitness(parameters):
                 folder_path = os.path.join(experiments_path, folder)
                 file_path = os.path.join(folder_path, "output.csv")
                 if os.path.exists(file_path):
-                    f, t = sort_by_fitness_and_time(pd.read_csv(file_path))
-                    print("pair (fitness, time): ", (f, t))
-                    fitness_time_pairs.append((f, t))
+                    f, c = sort_by_fitness_and_coverage(pd.read_csv(file_path))
+                    print("pair (fitness, coverage): ", (f, c))
+                    fitness_coverage_pairs.append((f, c))
 
-            best_fitness, time = get_average(fitness_time_pairs)
+            best_fitness, coverage = get_average(fitness_coverage_pairs)
             print("average fitness: ", best_fitness)
-            print("average time: ", time)
+            print("average coverage: ", coverage)
         except Exception as e:
             print(e)
             return 1
 
-    return int(best_fitness), int(time)
+    return int(best_fitness), int(coverage)
 
 
-def sort_by_fitness_and_time(df):
+def sort_by_fitness_and_coverage(df):
     df = df[df['iteration'] == df['iteration'].max()]
-    df = df.sort_values(by=['fitness', 'time'], ascending=[False, True])
+    df = df.sort_values(by=['fitness', 'coverage'], ascending=[False, False])
     fitness_column = df['fitness']
-    time_column = df['time']
+    coverage_column = df['coverage']
     best_fitness = 1
-    time = 0
+    coverage = 0
     for i in range(len(fitness_column)):
         if fitness_column.iloc[i] != "fitness":
             best_fitness = fitness_column.iloc[i]
             break
 
-    for i in range(len(time_column)):
-        if time_column.iloc[i] != "time":
-            time = time_column.iloc[i]
+    for i in range(len(coverage_column)):
+        if coverage_column.iloc[i] != "coverage":
+            coverage = coverage_column.iloc[i]
             break
 
-    return int(best_fitness), int(time)
+    return int(best_fitness), int(coverage)
 
 
-def get_average(fitness_time_pairs):
+def get_average(fitness_coverage_pairs):
     average_fitness = 0
-    average_time = 0
-    for pair in fitness_time_pairs:
+    average_coverage = 0
+    for pair in fitness_coverage_pairs:
         average_fitness += pair[0]
-        average_time += pair[1]
+        average_coverage += pair[1]
 
-    average_fitness /= len(fitness_time_pairs)
-    average_time /= len(fitness_time_pairs)
+    average_fitness /= len(fitness_coverage_pairs)
+    average_coverage /= len(fitness_coverage_pairs)
 
-    return average_fitness, average_time
+    return average_fitness, average_coverage
